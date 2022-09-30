4acb8c6ffb7ee92855e300a270020e37ab6cb004
Olesya Solovyova <soe611@yandex.ru>
2022-09-30 21:47:05 +0300

    edits Task58 HW

--- Sem8Task58 HW/Program.cs
+++ Sem8Task58 HW/Program.cs
@@ -11,7 +11,18 @@
 
 int[,] MatrixMultiply(int[,] matr1, int[,] matr2)
 {
-
+    int[,] matrMult = new int[matr1.GetLength(0), matr2.GetLength(1)];
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr2.GetLength(0); k++)
+            {
+                matrMult[i,j] += matr1[i,k] * matr2[k,j];
+            }
+        }
+    }
+    return matrMult;
 }
 
 //создаётся рандомный двумерный массив с заданными пользователем границами
@@ -39,15 +50,18 @@ void Print2DArray(int[,] matr)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]} ");
+            Console.Write($"{matr[i, j]}\t");
         }
         Console.WriteLine();
     }
 }
 
-int[,] matrix1 = Fill2DArray(4, 4, 0, 9);
+ Console.WriteLine("Созданная матрица №1");
+int[,] matrix1 = Fill2DArray(2, 2, 0, 9);
 Print2DArray(matrix1);
-int[,] matrix2 = Fill2DArray(4, 4, 0, 9);
+ Console.WriteLine("Созданная матрица №2");
+int[,] matrix2 = Fill2DArray(2, 2, 0, 9);
 Print2DArray(matrix2);
+ Console.WriteLine("Произведение матриц №1 и №2");
 int[,] matrMult = MatrixMultiply(matrix1, matrix2);
 Print2DArray(matrMult);
